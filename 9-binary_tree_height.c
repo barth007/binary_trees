e56d0967f27b753b3a0ce66b11ad20c519c573d9
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-06-02 09:46:58 +0000

    updated task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,14 +7,20 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t count;
+	size_t left_height, right_height;
 
-	count = 0;
-	if (tree == NULL)
-		return (0);
-	if (tree->left)
-		count = 1 + binary_tree_height(tree->left);
-	if (tree->right)
-		count = 1 + binary_tree_height(tree->right);
-	return (count);
+	left_height = 0;
+	right_height = 0;
+	if (tree)
+	{
+		left_height = 1 + binary_tree_height(tree->left);
+		right_height = 1 + binary_tree_height(tree->right);
+		if (tree->left == NULL && tree->right ==  NULL)
+			return (0);
+		if (left_height > right_height)
+			return (left_height);
+		else
+			return (right_height);
+	}
+	return (0);
 }
